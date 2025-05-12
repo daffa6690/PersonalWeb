42ba8f35f24a0ff3e1fc362427a5d8f70074bbbb
daffa6690 <daffadaffamdf@gmail.com>
2025-05-12 08:30:57 +0700

    final update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import pp from "../assets/pp.jpeg"
+import logoWa from "../assets/WhatsApp.svg.webp"
 
 function Hero() {
   return (
@@ -74,7 +75,7 @@ function Hero() {
                 className="w-full sm:w-auto bg-green-500 hover:bg-green-600 text-white px-8 py-3 rounded-lg transition-colors flex items-center justify-center font-medium"
               >
                 <img
-                  src="src\assets\WhatsApp.svg.webp"
+                  src={logoWa}
                   alt="WhatsApp"
                   className="w-5 h-5 mr-2"
                 />
